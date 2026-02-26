e0e4a8b4ae40557355a901d488e69b6e43f8b0ff
pavithrapn49 <pavithrapn49@gmail.com>
2026-02-26 14:17:30 +0530

    Add root route to confirm server is running successfully

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ import borrowRoutes from "./routes/borrowRoutes.js";
 dotenv.config();
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Library Management Backend Running Successfully 🚀");
+});
+
 // CORS
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
