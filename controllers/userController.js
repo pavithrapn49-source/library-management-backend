91ce0378f2002af74748c6fa689569bf08111a0e
pavithrapn49 <pavithrapn49@gmail.com>
2026-04-20 21:37:21 +0530

    fix: correct import case for User model in userController and authMiddleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/user.js";
 import jwt from "jsonwebtoken";
 
 const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
