2da06b4a6a38d1e7b24588086edab0272fe89be0
pavithrapn49 <pavithrapn49@gmail.com>
2026-04-23 01:55:09 +0530

    fix: remove trailing slash from production frontend CORS origin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://react-library-management-frze.vercel.app/",
+      "https://react-library-management-frze.vercel.app",
     ],
     credentials: true,
   })
